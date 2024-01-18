870b51ec314c6e803c1cfb40e9e6160ef20e55fa
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2024-01-18 21:13:27 +0530

    mini syntax change

--- api/server.js
+++ api/server.js
@@ -16,14 +16,14 @@ const Data= require('./models/Data')
 
 mongoose.connect(process.env.URI);
 
-const __dirname= path.resolve();
+const _dirname= path.resolve();
 
 const app= express();
 
-app.use(express.static(path.join(__dirname,'client','dist')))
+app.use(express.static(path.join(_dirname,'client','dist')))
 
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+    res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'))
 })
 
 app.use(express.json());
